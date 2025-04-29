4e729e69e1bb4b8b6bf71f6267ea15763a0de869
jamesfoday <jamesfodayofficial@gmail.com>
2025-04-29 21:30:54 +0200

    Clean up Atatus test error and finalize performance monitoring setup
    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
+import * as atatus from 'atatus-spa';
+atatus.config('55d8a5110d4f4a349dc65149ea4afdcb').install();
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
